5132af123b8d303340c5dd3647a90e406d8dde36
伊舞寻风 <yiwuxunfeng@yiwuxunfengdeiMac.local>
2017-04-21 11:03:07 +0800

    提示窗UI（已可用）

--- MangroveTree/MangroveTree/BaseAlertViewController.h
+++ MangroveTree/MangroveTree/BaseAlertViewController.h
@@ -13,9 +13,9 @@
  */
 typedef NS_ENUM(NSInteger,BaseAlertType)
 {
-    AlertType_default            = 0,
-    AlertType_callTask           = 1,
-    AlertType_pushNoti           = 2,
+    AlertType_systemAutoCancelTask              = 0, // 系统自动取消任务提示
+    AlertType_callTaskComplete                  = 1, // 呼叫服务完成提示
+    AlertType_cancelTaskReason                  = 2, // 取消呼叫任务弹窗
 };
 
 @interface BaseAlertViewController : UIViewController
@@ -25,6 +25,7 @@ typedef NS_ENUM(NSInteger,BaseAlertType)
 @property (weak, nonatomic) IBOutlet UIView *backGroundView;
 @property (weak, nonatomic) IBOutlet UIImageView *headImage;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *detailLabel;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIButton *comfirmButton;
 @property (weak, nonatomic) IBOutlet UIButton *cancelButton;
@@ -32,7 +33,13 @@ typedef NS_ENUM(NSInteger,BaseAlertType)
 @property (nonatomic, assign) NSInteger selectTable;
 
 // 根据标题等初始化
-+ (instancetype)initWithHeadTitle:(NSString *)headTitle andWithCheckTitles:(NSArray *)checkTitles andWithButtonTitles:(NSArray *)buttonTitles andWithHeadImage:(UIImage *)image;
++ (instancetype)initWithHeadTitle:(NSString *)headTitle andWithDetail:(NSString *)detail andWithCheckTitles:(NSArray *)checkTitles andWithButtonTitles:(NSArray *)buttonTitles andWithHeadImage:(UIImage *)image;
+
+// 根据使用类型初始化(使用tableView选择框的)
++ (instancetype)alertWithAlertType:(BaseAlertType)alertType andWithCheckTitles:(NSArray *)checkTitles andWithWaiterId:(NSString *)waiterId;
+
+// 根据使用类型初始化
++ (instancetype)alertWithAlertType:(BaseAlertType)alertType andWithWaiterId:(NSString *)waiterId;
 
 // 添加弹窗button点击事件 可传nil会默认返回 （一个按钮）
 - (void)addTarget:(id)target andWithComfirmAction:(SEL)action;
@@ -40,14 +47,14 @@ typedef NS_ENUM(NSInteger,BaseAlertType)
 // 添加弹窗button点击事件 可传nil会默认返回 （两个按钮）
 - (void)addTarget:(id)target andWithComfirmAction:(SEL)action1 andWithCancelAction:(SEL)action2;
 
-// 设置按钮两边边距
-- (void)setButtonLeadingTrailingContants:(CGFloat)contant;
-
 // 设置headimage大小
 - (void)setHeadImageSizes:(CGFloat)height;
 
 // 设置label，button文字大小
-- (void)setHeadTitleFont:(UIFont *)headTitleFont andWithButtonTitleFont:(UIFont *)buttonTitleFont;
+- (void)setHeadTitleFont:(UIFont *)headTitleFont andDetailFont:(UIFont *)detailFont andWithButtonTitleFont:(UIFont *)buttonTitleFont;
+
+// 设置headTitle 文字模式（居左，居中等）
+- (void)setHeadTitleTextAlignment:(NSTextAlignment)textAlignment;
 
 @end
 
