c91e34cac8aa86e91eceab5d0f2e853176367899
chao liu <793966766@qq.com>
2017-05-13 15:04:08 +0800

    定位点偏离

--- MangroveTree/MangroveTree/ViewController/FengMap/FMView/FMView.m
+++ MangroveTree/MangroveTree/ViewController/FengMap/FMView/FMView.m
@@ -1019,8 +1019,15 @@ extern NSString* FMModelSelected;
 //        [_locationMarker locateWithGeoCoord:mapsCoord.coord];
 #warning 这里需要区分是否有定位信息
 //        [_locationMarker locateWithGeoCoord:[self getDefaultMapCoord].coord];
+        
+        if (mapCoord.mapID != kOutdoorMapID)
+        {
+            [_locationMarker locateWithGeoCoord:FMKGeoCoordMake(1, mapCoord.coord.mapPoint)];
+        }else
+        {
         // 现场使用的方法
-        [_locationMarker locateWithGeoCoord:mapCoord.coord];
+            [_locationMarker locateWithGeoCoord:mapCoord.coord];
+        }
         _locationMarker.hidden = NO;
         
 	}
